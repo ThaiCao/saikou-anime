7bcb1df57c71f07eb779a7f2233f8440c080d38f
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2022-03-20 01:45:40 +0530

    - Made Player Settings UI - Added more drawables

--- app/src/main/java/ani/saikou/settings/PlayerSettingsActivity.kt
+++ app/src/main/java/ani/saikou/settings/PlayerSettingsActivity.kt
@@ -1,8 +1,13 @@
 package ani.saikou.settings
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updateLayoutParams
 import ani.saikou.databinding.ActivityPlayerSettingsBinding
+import ani.saikou.initActivity
+import ani.saikou.navBarHeight
+import ani.saikou.statusBarHeight
 
 class PlayerSettingsActivity : AppCompatActivity() {
     lateinit var binding : ActivityPlayerSettingsBinding
@@ -11,6 +16,10 @@ class PlayerSettingsActivity : AppCompatActivity() {
         binding = ActivityPlayerSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        initActivity(this)
+        binding.playerSettingsContainer.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            topMargin = statusBarHeight
+            bottomMargin = navBarHeight
+        }
     }
 }
