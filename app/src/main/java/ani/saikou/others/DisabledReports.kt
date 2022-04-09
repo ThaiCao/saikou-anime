3e7d04ea310078f9c015f87f4d223743fae0666a
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2022-04-10 03:51:37 +0530

    test gitignore

--- app/src/main/java/ani/saikou/others/DisabledReports.kt
+++ app/src/main/java/ani/saikou/others/DisabledReports.kt
@@ -1,4 +1,4 @@
 package ani.saikou.others
 
-const val DisabledReports=true
+const val DisabledReports=false
 //Setting this to false, will allow sending crash reports to Saikou's Firebase Crashlytics
