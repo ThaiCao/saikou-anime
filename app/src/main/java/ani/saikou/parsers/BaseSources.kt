96f82fff55dc9937088c0e5be13bf326a9fda395
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2023-03-07 13:29:17 +0530

    Fixy foxy

--- app/src/main/java/ani/saikou/parsers/BaseSources.kt
+++ app/src/main/java/ani/saikou/parsers/BaseSources.kt
@@ -13,7 +13,7 @@ abstract class WatchSources : BaseSources() {
     }
 
     suspend fun loadEpisodesFromMedia(i: Int, media: Media): MutableMap<String, Episode> {
-        return tryWithSuspend {
+        return tryWithSuspend(true) {
             val res = get(i).autoSearch(media) ?: return@tryWithSuspend mutableMapOf()
             loadEpisodes(i, res.link, res.extra)
         } ?: mutableMapOf()
@@ -22,7 +22,7 @@ abstract class WatchSources : BaseSources() {
     suspend fun loadEpisodes(i: Int, showLink: String, extra: Map<String, String>?): MutableMap<String, Episode> {
         val map = mutableMapOf<String, Episode>()
         val parser = get(i)
-        tryWithSuspend {
+        tryWithSuspend(true) {
             parser.loadEpisodes(showLink,extra).forEach {
                 map[it.number] = Episode(it.number, it.link, it.title, it.description, it.thumbnail, it.isFiller, extra = it.extra)
             }
@@ -39,7 +39,7 @@ abstract class MangaReadSources : BaseSources() {
     }
 
     suspend fun loadChaptersFromMedia(i: Int, media: Media): MutableMap<String, MangaChapter> {
-        return tryWithSuspend {
+        return tryWithSuspend(true) {
             val res = get(i).autoSearch(media) ?: return@tryWithSuspend mutableMapOf()
             loadChapters(i, res)
         } ?: mutableMapOf()
@@ -48,7 +48,7 @@ abstract class MangaReadSources : BaseSources() {
     suspend fun loadChapters(i: Int, show: ShowResponse): MutableMap<String, MangaChapter> {
         val map = mutableMapOf<String, MangaChapter>()
         val parser = get(i)
-        tryWithSuspend {
+        tryWithSuspend(true) {
             parser.loadChapters(show.link, show.extra).forEach {
                 map[it.number] = MangaChapter(it)
             }
