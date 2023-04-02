6e11ab6a3ebae55639618d64db7d0b9b352d7a38
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2023-04-02 18:26:01 +0530

    Fix #629

--- app/src/main/java/ani/saikou/others/MalScraper.kt
+++ app/src/main/java/ani/saikou/others/MalScraper.kt
@@ -8,9 +8,7 @@ import kotlinx.coroutines.withTimeout
 
 object MalScraper {
     private val headers = mapOf(
-        "accept" to " text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding" to " gzip, deflate, br",
-        "accept-language" to " en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7"
+        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     )
 
     suspend fun loadMedia(media: Media) {
