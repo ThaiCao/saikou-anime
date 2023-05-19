2ea757344bac06eab4c01bb71539efc5b8b1b103
Jeremy <74879119+jerry08@users.noreply.github.com>
2023-05-19 00:38:15 -0400

    Fix streamsb (#673)
    * Fix gogo extractor
    
    * Fix streamsb

--- app/src/main/java/ani/saikou/parsers/anime/extractors/StreamSB.kt
+++ app/src/main/java/ani/saikou/parsers/anime/extractors/StreamSB.kt
@@ -7,14 +7,15 @@ import ani.saikou.findBetween
 import ani.saikou.parsers.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlin.math.floor
+import kotlin.random.Random
 
 class StreamSB(override val server: VideoServer) : VideoExtractor() {
     override suspend fun extract(): VideoContainer {
         val videos = mutableListOf<Video>()
         val id = server.embed.url.let { it.findBetween("/e/", ".html") ?: it.split("/e/")[1] }
-        val source = client.get("https://raw.githubusercontent.com/saikou-app/mal-id-filler-list/main/sb.txt").text
         val jsonLink =
-            "$source/${bytesToHex("||$id||||streamsb".toByteArray())}/"
+            "https://sbani.pro/375664356a494546326c4b797c7c6e756577776778623171737/${encode(id)}"
         val json = client.get(jsonLink, mapOf("watchsb" to "sbstream")).parsed<Response>()
         if (json.statusCode == 200) {
             videos.add(Video(null, VideoType.M3U8, FileUrl(json.streamData!!.file, defaultHeaders)))
@@ -22,17 +23,24 @@ class StreamSB(override val server: VideoServer) : VideoExtractor() {
         return VideoContainer(videos)
     }
 
-    private val hexArray = "0123456789ABCDEF".toCharArray()
+    private val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
-    private fun bytesToHex(bytes: ByteArray): String {
-        val hexChars = CharArray(bytes.size * 2)
-        for (j in bytes.indices) {
-            val v = bytes[j].toInt() and 0xFF
-
-            hexChars[j * 2] = hexArray[v ushr 4]
-            hexChars[j * 2 + 1] = hexArray[v and 0x0F]
+    private fun encode(id: String): String {
+        val code = "${makeId()}||${id}||${makeId()}||streamsb"
+        val sb = StringBuilder()
+        val arr = code.toCharArray()
+        for (j in arr.indices) {
+            sb.append(arr[j].code.toString().toInt(10).toString(16))
         }
-        return String(hexChars)
+        return sb.toString()
+    }
+
+    private fun makeId(): String {
+        val sb = StringBuilder()
+        for (j in 0..12) {
+            sb.append(alphabet[(floor(Random.nextDouble() * alphabet.length)).toInt()])
+        }
+        return sb.toString()
     }
 
     @Serializable
